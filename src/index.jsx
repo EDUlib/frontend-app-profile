0a9ee16e4b4ceb39b110a378aa60aa47c00aaf6b
Robert Raposa <rraposa@edx.org>
2019-01-18 16:39:18 -0500

    fixup: move handleAllTrackEvents.

--- src/index.jsx
+++ src/index.jsx
@@ -6,15 +6,11 @@ import { Provider } from 'react-redux';
 import SiteFooter from '@edx/frontend-component-footer';
 
 import apiClient from './data/apiClient';
+import { handleTrackEvents } from './analytics';
 import UserAccount from './components/UserAccount';
 import store from './data/store';
 import FooterLogo from '../assets/edx-footer.png';
 import './App.scss';
-
-const trackEvent = (eventName, properties) => {
-  window.analytics.track(eventName, properties);
-  console.log(properties);
-};
 
 const App = () => (
   <Provider store={store}>
@@ -42,7 +38,7 @@ const App = () => (
           redditUrl={process.env.REDDIT_URL}
           appleAppStoreUrl={process.env.APPLE_APP_STORE_URL}
           googlePlayUrl={process.env.GOOGLE_PLAY_URL}
-          trackEvent={trackEvent}
+          handleAllTrackEvents={handleTrackEvents}
         />
       </div>
     </Router>
